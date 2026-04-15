200180f0fa2e1a8a43387dde7a29022d14f86544
Sneglium <faustawker@gmail.com>
2026-04-15 12:50:25 +1200

    Separate group for toolbox-allowed tools removed

--- scripts/hand_tools.lua
+++ scripts/hand_tools.lua
@@ -5,7 +5,7 @@ if core.settings: get_bool('crafttools.file_enabled', true) then
 	crafttools.register_tool('crafttools:file', core.settings: get 'crafttools.file_max_uses' or 150, {
 		description = crafttools.gettext 'Metal File',
 		inventory_image = 'crafttools_file.png',
-		groups = {craft_tool_file = 1, craft_tool_file_tb = 1}
+		groups = {craft_tool_file = 1}
 	})
 
 	if enable_recipes then
@@ -24,7 +24,7 @@ if core.settings: get_bool('crafttools.drill_enabled', true) then
 	crafttools.register_tool('crafttools:drill', core.settings: get 'crafttools.drill_max_uses' or 200, {
 		description = crafttools.gettext 'Hand Drill',
 		inventory_image = 'crafttools_drill.png',
-		groups = {craft_tool_drill = 1, craft_tool_drill_tb = 1}
+		groups = {craft_tool_drill = 1}
 	})
 
 	if enable_recipes then
@@ -43,7 +43,7 @@ if core.settings: get_bool('crafttools.hammer_enabled', true) then
 	crafttools.register_tool('crafttools:hammer', core.settings: get 'crafttools.hammer_max_uses' or 250, {
 		description = crafttools.gettext 'Small Hammer',
 		inventory_image = 'crafttools_hammer.png',
-		groups = {craft_tool_hammer = 1, craft_tool_hammer_tb = 1}
+		groups = {craft_tool_hammer = 1}
 	})
 
 	if enable_recipes then
@@ -62,7 +62,7 @@ if core.settings: get_bool('crafttools.cutters_enabled', true) then
 	crafttools.register_tool('crafttools:cutters', core.settings: get 'crafttools.cutters_max_uses' or 250, {
 		description = crafttools.gettext 'Metal Cutters',
 		inventory_image = 'crafttools_cutters.png',
-		groups = {craft_tool_cutters = 1, craft_tool_cutters_tb = 1}
+		groups = {craft_tool_cutters = 1}
 	})
 
 	if enable_recipes then
@@ -81,7 +81,7 @@ if core.settings: get_bool('crafttools.wrench_enabled', true) then
 	crafttools.register_tool('crafttools:wrench', core.settings: get 'crafttools.wrench_max_uses' or 200, {
 		description = crafttools.gettext 'Wrench',
 		inventory_image = 'crafttools_wrench.png',
-		groups = {craft_tool_wrench = 1, craft_tool_wrench_tb = 1}
+		groups = {craft_tool_wrench = 1}
 	})
 
 	if enable_recipes then
@@ -129,7 +129,7 @@ if core.settings: get_bool('crafttools.saw_enabled', true) then
 	crafttools.register_tool('crafttools:saw', core.settings: get 'crafttools.saw_max_uses' or 250, {
 		description = crafttools.gettext 'Hand Saw',
 		inventory_image = 'crafttools_saw.png',
-		groups = {craft_tool_saw = 1, craft_tool_saw_tb = 1}
+		groups = {craft_tool_saw = 1}
 	})
 
 	if enable_recipes then
@@ -181,8 +181,8 @@ if core.settings: get_bool('crafttools.enable_toolbox', true) then
 		core.register_craft {
 			type = 'shapeless',
 			recipe = {
-				CraftTool 'group:craft_tool_file_tb': consume(), CraftTool 'group:craft_tool_drill_tb': consume(), CraftTool 'group:craft_tool_hammer_tb': consume(),
-				CraftTool 'group:craft_tool_cutters_tb': consume(), CraftTool 'group:craft_tool_wrench_tb': consume(), CraftTool 'group:craft_tool_saw_tb': consume(),
+				CraftTool 'group:craft_tool_file': consume(), CraftTool 'group:craft_tool_drill': consume(), CraftTool 'group:craft_tool_hammer': consume(),
+				CraftTool 'group:craft_tool_cutters': consume(), CraftTool 'group:craft_tool_wrench': consume(), CraftTool 'group:craft_tool_saw': consume(),
 				'default:chest'
 				
 			},
@@ -220,8 +220,8 @@ if core.settings: get_bool('crafttools.enable_toolbox', true) then
 	core.register_craft_predict(function (crafted, player, old_craft_grid, craft_inv)
 		if crafted: get_name() == 'crafttools:toolbox' then
 			for index, item in ipairs(old_craft_grid) do
+				if item: get_name() == 'crafttools:toolbox' then return '' end
 				local unbreakable = core.get_item_group(itemname, 'crafttools_tool_unbreakable') == 1
-				
 				if unbreakable then return '' end
 			end
 		end
